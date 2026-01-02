be3bf7319c15c8b31799390dec206e93a79e5fca
lzif <lukyfriendly@gmail.com>
2026-01-02 05:04:00 +0000

    fix: delete esbuild plugin which causes out/ folder not to be published to npm

--- build.ts
+++ build.ts
@@ -1,13 +1,11 @@
 import dts from "bun-plugin-dts";
-import esbuildCleanOutdir from "esbuild-plugin-clean-outdir";
 
 await Bun.build({
-	entrypoints: ["./src/index.ts"],
-	outdir: "./out",
-	minify: true,
-	splitting: true,
-	sourcemap: true,
-	target: "browser",
-	tsconfig: "./tsconfig.json",
-	plugins: [esbuildCleanOutdir(), dts()],
+  entrypoints: ["./src/index.ts"],
+  outdir: "./out",
+  minify: true,
+  sourcemap: true,
+  target: "browser",
+  tsconfig: "./tsconfig.json",
+  plugins: [dts()],
 });
